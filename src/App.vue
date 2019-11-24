fd2e0f93beb7c08ab8f77200b1c38bbd065b5efe
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-11-24 15:37:02 +0100

    recipe route added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link :to="{ name: 'Home' }">Home</router-link>
+    <router-link to="/recipe">Recipe</router-link>
     <router-view/>
   </div>
 </template>
---
+++ src/components/RecipeComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="recipe">
+    <h1>Recipe</h1>
+    <p>
+     Add some flavor
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recipe'
+}
+</script>
